6b57c18f6c6fec93a1b2eb461b2015aa26d3cfa0
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-31 10:41:39 +0100

    fetch

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Tu códgigo aquí
+const image = document.querySelector('img');
+const jokeDIV = document.querySelector('#display-joke');
+const button = document.querySelector('#get-joke');
+
+button.addEventListener('click', function(){
+    getRandomJoke();
+})
+
+function getRandomJoke(){
+    const url = 'https://api.chucknorris.io/jokes/random'
+
+    fetch(url).
+    then(response => response.json()).
+     then(data => jokeDIV.innerHTML = data.value)       
+    
+}
+
+function onerror(){
+     jokeDIV.textContent = 'There was an error!';  
+}
